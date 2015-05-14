f3dd1788780e63dc75b6e51fae99bb9d61ac0804
Richard Cyganiak <richard@cyganiak.de>
2015-05-14 17:26:51 +0100

    Add concept URI to suggester service results

--- src/main/java/org/topbraid/mauiserver/tagger/RecommendationResult.java
+++ src/main/java/org/topbraid/mauiserver/tagger/RecommendationResult.java
@@ -8,39 +8,50 @@ import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
 public class RecommendationResult {
-	private List<String> recommendations;
-	private double[] probabilities;
+	private List<String> ids;
+	private List<String> titles;
+	private List<Double> probabilities;
 	
 	public RecommendationResult(List<Topic> mauiTopics) {
-		recommendations = new ArrayList<String>(mauiTopics.size());
-		probabilities = new double[mauiTopics.size()];
+		ids = new ArrayList<String>(mauiTopics.size());
+		titles = new ArrayList<String>(mauiTopics.size());
+		probabilities = new ArrayList<Double>(mauiTopics.size());
 		for (int i = 0; i < mauiTopics.size(); i++) {
 			Topic t = mauiTopics.get(i);
-			recommendations.add(t.getTitle());
-			probabilities[i] = t.getProbability();
+			ids.add(t.getId());
+			titles.add(t.getTitle());
+			probabilities.add(t.getProbability());
 		}
 	}
 	
 	public List<String> getRecommendations() {
-		return recommendations;
+		return ids;
+	}
+	
+	public String getTitle(String recommendation) {
+		int i = ids.indexOf(recommendation);
+		if (i == -1) return null;
+		return titles.get(i);
 	}
 	
 	public double getProbability(String recommendation) {
-		int i = recommendations.indexOf(recommendation);
+		int i = ids.indexOf(recommendation);
 		if (i == -1) return 0;
-		return probabilities[i];
+		return probabilities.get(i);
 	}
 	
 	@Override
 	public String toString() {
 		StringBuffer s = new StringBuffer();
 		s.append('[');
-		for (int i = 0; i < recommendations.size(); i++) {
+		for (int i = 0; i < ids.size(); i++) {
 			if (i > 0) s.append(", ");
 			s.append('"');
-			s.append(recommendations.get(i));
+			s.append(titles.get(i));
+			s.append("\"|\"");
+			s.append(ids.get(i));
 			s.append("\"(");
-			s.append(probabilities[i]);
+			s.append(probabilities.get(i));
 			s.append(')');
 		}
 		s.append(']');
@@ -49,10 +60,11 @@ public class RecommendationResult {
 	
 	public void toJSON(ObjectNode root) {
 		ArrayNode results = root.arrayNode();
-		for (int i = 0; i < recommendations.size(); i++) {
+		for (int i = 0; i < ids.size(); i++) {
 			ObjectNode result = root.objectNode();
-			result.put("label", recommendations.get(i));
-			result.put("probability", probabilities[i]);
+			result.put("id", ids.get(i));
+			result.put("label", titles.get(i));
+			result.put("probability", probabilities.get(i));
 			results.add(result);
 		}
 		root.set("topics", results);
