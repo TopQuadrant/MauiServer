636b3b9c4141fb07773ff53a94ee185529be3db0
Richard Cyganiak <richard@cyganiak.de>
2017-08-11 21:05:54 +0100

    Add a Portuguese stopwords list and pretend that a Spanish stemmer works for Pt

--- src/main/java/org/topbraid/mauiserver/lang/pt/PortugueseStemmer.java
+++ src/main/java/org/topbraid/mauiserver/lang/pt/PortugueseStemmer.java
@@ -1,9 +1,66 @@
 package org.topbraid.mauiserver.lang.pt;
 
 import com.entopix.maui.stemmers.SpanishStemmer;
+import com.entopix.maui.stemmers.Stemmer;
 
-public class PortugueseStemmer extends SpanishStemmer {
+/**
+ * Not a portuguese stemmer. Actually just
+ * some code copied from {@link SpanishStemmer}.
+ */
+public class PortugueseStemmer extends Stemmer {
 
 	private static final long serialVersionUID = 1L;
+
+	public String stem(String word) {
+		return stemSpanish(word);
+	}
+	
+    /*  Spanish stemmer tring to remove inflectional suffixes */
+    public String stemSpanish(String word) {
+
+        int len = word.length() - 1;
+
+        if (len > 3) {
+
+            word = removeSpanishAccent(word);
+
+            if (word.endsWith("eses")) {
+                //  corteses -> cortÈs
+                word = word.substring(0, len - 1);
+                return word;
+            }
+
+            if (word.endsWith("ces")) {
+                //  dos veces -> una vez
+                word = word.substring(0, len - 2);
+                word = word + 'z';
+                return word;
+            }
+
+            if (word.endsWith("os") || word.endsWith("as") || word.endsWith("es")) {
+                //  ending with -os, -as  or -es
+                word = word.substring(0, len - 1);
+                return word;
+
+            }
+            if (word.endsWith("o") || word.endsWith("a") || word.endsWith("e")) {
+                //  ending with  -o,  -a, or -e
+                word = word.substring(0, len - 1);
+                return word;
+            }
+
+        }
+        return word;
+    }
+
+    private String removeSpanishAccent(String word) {
+        word = word.replaceAll("‡|·|‚|‰", "a");
+        word = word.replaceAll("Ú|Û|Ù|ˆ", "o");
+        word = word.replaceAll("Ë|È|Í|Î", "e");
+        word = word.replaceAll("˘|˙|˚|¸", "a");
+        word = word.replaceAll("Ï|Ì|Ó|Ô", "a");
+
+        return word;
+    }
 
 }
--- src/main/java/org/topbraid/mauiserver/lang/pt/StopwordsPortuguese.java
+++ src/main/java/org/topbraid/mauiserver/lang/pt/StopwordsPortuguese.java
@@ -1,9 +1,39 @@
 package org.topbraid.mauiserver.lang.pt;
 
-import com.entopix.maui.stopwords.StopwordsSpanish;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
-public class StopwordsPortuguese extends StopwordsSpanish {
+import com.entopix.maui.stopwords.Stopwords;
 
-    private static final long serialVersionUID = 1L;
+/**
+ * Portuguese stopwords filter, based on stopwords loaded from a
+ * file we found on the internet.
+ * 
+ * @see https://gist.github.com/alopes/5358189
+ */
+public class StopwordsPortuguese extends Stopwords {
 
+	private static final long serialVersionUID = 1L;
+
+	public StopwordsPortuguese() {
+		super(readStopwordsFile());
+	}
+
+	private static List<String> readStopwordsFile() {
+		ClassLoader cl = StopwordsPortuguese.class.getClassLoader();
+		try (BufferedReader br = new BufferedReader(
+				new InputStreamReader(cl.getResourceAsStream("/stopwords-pt.txt")))) {
+			List<String> result = new ArrayList<>();
+			String line = null;
+			while ((line = br.readLine()) != null) {
+				result.add(line.trim());
+			}
+			return result;
+		} catch (IOException ex) {
+			throw new RuntimeException(ex);
+		}
+	}
 }
